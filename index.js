cace4a41746e2a9594d332712364d325f6caf07b
TANJUMAJERIN <bsse1312@iit.du.ac.bd>
2025-01-19 04:18:58 +0600

    hehe

--- index.js
+++ index.js
@@ -4,28 +4,7 @@ const app = express();
 //const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hellooo World!');
 });
 module.exports = app; 
 
-// let server;
-
-// if (process.env.NODE_ENV == 'test') {
-//   // Only start the server if not in test environment
-//   server = app.listen(PORT, () => {
-//     console.log(`Server is running on http://localhost:${PORT}`);
-//   });
-// }
-
-// module.exports = { app, server }; // Export app and server for tests
-/////jikjn
-
-// //cc
-// const express = require('express');
-// const app = express();
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!');
-// });
-
-// module.exports = app; 
