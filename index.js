93fdceb2f824975441d903e4d269b6faf43dad99
TANJUMAJERIN <bsse1312@iit.du.ac.bd>
2025-01-19 02:56:29 +0600

    hehe

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
-// const express = require('express');
-// const app = express();
 
-// const PORT = 3000;
-
-// app.get('/', (req, res) => {
-//     res.send('Hello World');
-// });
-
-// app.listen(PORT, () => {
-//     console.log(`Server is running on http://localhost:${PORT}`);
-// });
-////
 const express = require('express');
 const app = express();
-const PORT = 3000;
+//const PORT = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+module.exports = app; 
+// let server;
 
-let server;
+// if (process.env.NODE_ENV == 'test') {
+//   // Only start the server if not in test environment
+//   server = app.listen(PORT, () => {
+//     console.log(`Server is running on http://localhost:${PORT}`);
+//   });
+// }
 
-if (process.env.NODE_ENV == 'test') {
-  // Only start the server if not in test environment
-  server = app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-  });
-}
-
-module.exports = { app, server }; // Export app and server for tests
+// module.exports = { app, server }; // Export app and server for tests
 /////jikjn
+
+// //cc
+// const express = require('express');
+// const app = express();
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
+
+// module.exports = app; 
--- test/index.test.js
+++ test/index.test.js
@@ -1,16 +1,6 @@
-// const request = require('supertest');
-// const app = require('../index'); // Adjust path if necessary
-
-// describe('GET /', () => {
-//     it('should return Hello, World!', async () => {
-//         const res = await request(app).get('/');
-//         expect(res.statusCode).toBe(200);
-//         expect(res.text).toBe('Hello World');
-//     });
-// });
 
 const request = require('supertest');
-const { app, server } = require('../index'); // Import the app and server
+const app = require('../index');
 
 describe('GET /', () => {
   it('should return Hello World!', async () => {
@@ -18,13 +8,6 @@ describe('GET /', () => {
     expect(res.statusCode).toBe(200);
     expect(res.text).toBe('Hello World!');
   });
-});
 
-// Close the server after the tests
-// afterAll(done => {
-//   if (server) {
-//     server.close(done); // Use the server instance to close
-//   } else {
-//     done(); // If no server, just call done
-//   }
-// });
+
+})
