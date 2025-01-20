5e231e5d541f0247a2b95d9d9fa2033ff8714756
TANJUMAJERIN <bsse1312@iit.du.ac.bd>
2025-01-20 13:03:26 +0600

    hobenaa

--- test/app.test.js
+++ test/app.test.js
@@ -6,7 +6,7 @@ describe('GET /', () => {
   it('should return Hello World!', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toBe(200);
-    expect(res.text).toBe('Helllo World!');
+    expect(res.text).toBe('Hello World!');
   });
 
 
